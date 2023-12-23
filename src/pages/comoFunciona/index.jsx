47af9001a3a5aec186c3d84b7a598419e1fdad23
bryaanls <bryanluccas@hotmail.com>
2023-12-23 14:30:43 -0300

    init second page

---
+++ src/pages/comoFunciona/index.jsx
@@ -0,0 +1,13 @@
+// import Accordion from "../../components/accordion";
+import Container from "../../components/container";
+import "./styles.css";
+
+const ComoFunciona = () => {
+  return (
+    <>
+      <Container></Container>
+    </>
+  );
+};
+
+export default ComoFunciona;
